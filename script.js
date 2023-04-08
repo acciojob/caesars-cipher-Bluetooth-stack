ab4eebcffd6e68cc6325d0baa116ce3f1fa2e196
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-04-08 13:03:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,12 +13,20 @@ const lookup = {
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
-  return ;//return decodedArr
+	let ind = 0;
+	for(let i = 0; i<encodedStr.length; i++){
+		if(encodedStr.charAt(i) in lookup){
+			decodedArr[ind++] = lookup[encodedStr.charAt(i)];
+		}
+		else{
+			decodedArr[ind++] = encodedStr.charAt(i);
+		}
+	}
+  return decodedArr;//return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 module.exports = rot13;
